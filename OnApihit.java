095a3a2664cb99da401eb75c3219f47b9b4f739c
rspapneja <33872930+rspapneja@users.noreply.github.com>
2017-11-23 01:17:35 +0530

    Add files via upload

---
+++ OnApihit.java
@@ -0,0 +1,12 @@
+package vollyclass;
+
+
+import com.android.volley.VolleyError;
+
+/**
+ * Created by Tejinder on 7/22/2016.
+ */
+public interface OnApihit {
+   void success(String Response, int index);
+    void error(VolleyError error, int index);
+}
