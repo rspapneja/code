e051a4e294fea03074f3f3e6a926eea81fe2116b
rspapneja <33872930+rspapneja@users.noreply.github.com>
2018-10-24 09:15:12 +0530

    Add files via upload

---
+++ Global_im.java
@@ -0,0 +1,17 @@
+package com.example.ajit.imageupload.util;
+
+/**
+ * Created by PC-DESTOP on 9/25/2017.
+ */
+
+public interface Global {
+
+    int REQUEST_CODE_LOCATION=1002;
+
+    int MULTI_LOC_STOR=1254;
+    int GalleryPicker=123;
+    int CameraPicker=124;
+    int REQUEST_CODE_STORAGE=1001;
+    String url="http://davcommunication.com/api/";
+
+}
